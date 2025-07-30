57b69d9668c2d60822811e45fe35fcce14196544
cwnnn <cangs026@gmail.com>
2025-07-30 03:13:52 +0300

    refactor: menu added for mobile compatibility

--- src/components/RcsHeader/RcsHeader.vue
+++ src/components/RcsHeader/RcsHeader.vue
@@ -1,25 +1,29 @@
-
 <template>
   <header class="header">
     <div class="logo">
-      <!-- Site title as a clickable link -->
       <router-link to="/" class="logo-link">Resul Can</router-link>
     </div>
-    
-    <nav class="nav-links">
-      <!-- Navigation links to other pages -->
+
+    <nav :class="['nav-links']">
       <router-link to="/" class="nav-link" exact-active-class="active">Home</router-link>
       <router-link to="/q" class="nav-link" active-class="active">Projects</router-link>
       <router-link to="/w" class="nav-link" active-class="active">About Me</router-link>
       <router-link to="/e" class="nav-link" active-class="active">Contact</router-link>
     </nav>
+
+   <button class="menu-toggle-button " @click="isOpen = !isOpen">☰</button>
+    <ul v-if="isOpen" class="md:hidden">
+      <li><router-link to="/" class="nav-link" exact-active-class="active" @click="isOpen = false">Home</router-link></li>
+      <li><router-link to="/q" class="nav-link" active-class="active" @click="isOpen = false">Projects</router-link></li>
+      <li><router-link to="/w" class="nav-link" active-class="active" @click="isOpen = false">About Me</router-link></li>
+      <li><router-link to="/e" class="nav-link" active-class="active" @click="isOpen = false">Contact</router-link></li>
+    </ul>
   </header>
 </template>
 
-<script>
-export default {
-  name: "Header"
-};
+<script setup>
+import { ref } from 'vue'
+const isOpen = ref(false);
 </script>
 
 <style scoped src="./RcsHeader.css">
