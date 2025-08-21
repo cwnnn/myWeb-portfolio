f91da11722f72fd2666a35f92d22bc9ee7ebca53
cwnnn <cangs026@gmail.com>
2025-08-21 21:39:29 +0300

    feat: update navigation links and add ProjectView component with project data

---
+++ src/data/projects.ts
@@ -0,0 +1,22 @@
+export const projects = [
+ { 
+    img: "/assets/projects/1.png", 
+    title: "Toxicity Game", 
+    desc: "A Turkish warrior's battle against zombies."
+  },
+  { 
+    img: "/assets/projects/2.png", 
+    title: "Bus Automation System", 
+    desc: "A system to automate bus operations and improve public transport efficiency."
+  },
+  { 
+    img: "/assets/projects/3.png", 
+    title: "Event Tracking System", 
+    desc: "A system to track and manage events efficiently."
+  },
+  { 
+    img: "/assets/projects/4.png", 
+    title: "myWeb-portfolio", 
+    desc: "A personal portfolio showcasing my projects and skills."
+  }
+];
