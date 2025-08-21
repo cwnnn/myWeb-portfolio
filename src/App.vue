cc8f4adf4226369be219a6ce7ea5a6797470c996
cwnnn <cangs026@gmail.com>
2025-08-22 01:26:11 +0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
+<template>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+  />
+  <div class="flex flex-col min-h-screen">
+    <RcsHeader />
+
+    <!-- Overlay div -->
+    <div id="overlay" class="fixed inset-0 z-10 bg-black bg-opacity-90 pointer-events-none"></div>
+
+    <main class="flex-1 relative z-20 text-white">
+      <router-view />
+    </main>
+
+    <RcsFooter class="relative z-20" />
+  </div>
+</template>
+
 <script setup>  
 import { onMounted } from 'vue';
 import RcsHeader from './components/RcsHeader/RcsHeader.vue';
+import RcsFooter from './components/RcsFooter/RcsFooter.vue';
 
 onMounted(() => {
   const overlay = document.getElementById('overlay');
@@ -12,23 +32,4 @@ onMounted(() => {
     overlay.style.webkitMaskImage = mask;
   });
 });
-</script>
-
-<template>
-  <link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-  />
-
-     <RcsHeader />
-     <div id="overlay" class="fixed inset-0 z-10 bg-black bg-opacity-90 pointer-events-none"></div>
-      <main class="relative z-20 text-white">
-       <router-view />
-      </main>
-</template>
-
-<style scoped>
-</style>
-  
-<script>
 </script>
